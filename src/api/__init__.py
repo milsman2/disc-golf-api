ee441ec821d7b5279bcce44eef886f9ded59adb4
milsman2 <milsman2@gmail.com>
2025-03-04 21:28:27 -0600

    Add private route for creating users.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,6 +2,7 @@
 Expose the router from the main module.
 """
 
-from src.api.main import router
+from src.api.main import api_router
+from src.api.deps import get_current_user, SessionDep
 
-__all__ = ["router"]
+__all__ = ["api_router", "get_current_user", "SessionDep"]
