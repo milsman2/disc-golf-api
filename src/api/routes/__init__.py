f659054f5d0dbd4eefa5c0108ef8fa573e1b44fc
milsman2 <milsman2@gmail.com>
2025-07-25 15:52:47 +0200

    fix: correct router import name for courses and update __all__ declaration

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -2,8 +2,8 @@
 Expose the routers to API main module
 """
 
-from src.api.routes.courses import router as courses_router
 from src.api.routes.course_layouts import router as course_layouts_router
+from src.api.routes.courses import router as course_router
 from src.api.routes.event_result import router as event_result_router
 from src.api.routes.event_sessions import router as event_sessions_router
 from src.api.routes.healthcheck import router as healthcheck_router
@@ -14,7 +14,7 @@ __all__ = [
     "healthcheck_router",
     "login_router",
     "course_layouts_router",
-    "courses_router",
+    "course_router",
     "private_router",
     "event_result_router",
     "event_sessions_router",
